d30ac381880e4b7d7951e749a9183ac1d6ef6d38
조안나 <lovesan@socialwith.co.kr>
2019-10-22 10:40:32 +0900

    no message

--- Socialwith_Mobile/App_Start/RouteConfig.cs
+++ Socialwith_Mobile/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace Socialwith_Mobile
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
